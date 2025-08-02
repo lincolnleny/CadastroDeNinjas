4bb4f1f0bda300a9b7e40fc4723fdcba9ddb6744
Lincoln Neves <lincolnleny@outlook.com>
2025-08-02 19:07:56 -0300

    "feat: Implementa modelagem inicial com Lombok, validações e repositórios
    - Modelagem do bando de dados, NinjaModel e MissoesModel concluídos, annotations feitas para relacionamento das tabelas."
    - Adiciona Lombok à classe para abstração de getters, setters e construtores"
    - Define email como campo UNIQUE
    - Cria o MissoesRepository para acesso ao banco de dados"

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -0,0 +1,36 @@
+package dev.java10x.CadastroDeNinjas.Ninjas;
+
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//Entity transforma a classe em uma entidade do banco de dados
+@Entity
+@Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class NinjaModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
+	private Long id;
+
+	private String nome;
+
+	@Column(unique = true)
+	private String email;
+
+	private int idade;
+
+	//ManyToOne - indica que um ninja pode estar associado a uma unica missão
+	@ManyToOne
+	@JoinColumn(name = "missoes_id") // Foreign key para a tabela de missoes
+	private MissoesModel missoes;
+
+
+}
